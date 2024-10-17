187b2524b9d5e5aeaad3bc585ffaa703c1c32c41
Micaele <micaellyvitoriac@gmail.com>
2024-10-17 20:30:52 -0300

    Adicionando a função "preenche_frota"

--- funcoes.py
+++ funcoes.py
@@ -19,3 +19,17 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
 
 
     return lista_posicoes
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+
+    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    if nome_navio in frota:
+
+        frota[nome_navio].append(posicoes_navio)
+
+    else:
+        frota[nome_navio] = [posicoes_navio]
+        
+    return frota
